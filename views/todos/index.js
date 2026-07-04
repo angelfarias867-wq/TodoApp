3e18327a9d99cf637b44956f31f9fe840d4ae002
angelfarias867-wq <angelfarias867@gmail.com>
2026-07-03 22:08:38 -0400

    intento del todos

--- views/todos/index.js
+++ views/todos/index.js
@@ -62,7 +62,7 @@ form.addEventListener('submit', async e => {
 					<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
 				</svg>
 			</button>
-			<p class="p-4 break-words grow">${data.text}</p>
+			<p class="p-4 wrap-break-word grow">${data.text}</p>
 		</div>
 		<button class="check-icon w-12 md:w-14 flex justify-center items-center cursor-pointer border-l border-slate-300 dark:border-slate-400 hover:bg-green-300 transition duration-300 easy-in-out">
 			<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 md:h-7 md:w-7 text-green-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
@@ -133,7 +133,7 @@ ul.addEventListener('click', async e => {
 							<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
 						</svg>
 					</button>
-					<p class="p-4 break-words grow">${todo.text}</p>
+					<p class="p-4 wrap-break-word grow">${todo.text}</p>
 				</div>
 				<button class="check-icon w-12 md:w-14 flex justify-center items-center cursor-pointer border-l border-slate-300 dark:border-slate-400 hover:bg-green-300 transition duration-300 easy-in-out">
 					<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 md:h-7 md:w-7 text-green-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
