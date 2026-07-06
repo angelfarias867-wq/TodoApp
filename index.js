7ec1310cf3c83eb5e2a342dc79e6d3b9323f7b2a
mariangel-ms <mariangelmartinez982@gmail.com>
2026-07-06 11:43:46 -0400

    Cambios para subir la pagina a render

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const app = require("./app.js");
 const PUERTO = 3000;
-require("dotenv").config();
 
 app.listen(PUERTO, () => {
   console.log("Esuchando en el puerto 3000");
